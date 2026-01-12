181dae0c4578e4993900280b84a5f88cf6c894ad
Thane <thabay0606@students.maquoketaschools.org>
2026-01-12 13:53:57 -0600

    I think I fixed the PerfectScore problem.

--- Course.cs
+++ Course.cs
@@ -4,7 +4,7 @@ public class Course
     // Add your code here
     public int MaxStudents = 25;
 
-    public const PerfectScore = 100;
+    public const int PerfectScore = 100;
     
     public int CourseLength(){
         // Add your code here
